a79b0c93f8ed94f85dd100a130c0a1f6b63a4f1f
Mike H <72580577+mekanik20@users.noreply.github.com>
2021-09-14 21:36:37 -0400

    Fixed rendering issues on browser

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class MovieView extends React.Component {
+export class MovieView extends React.Component {
 
 
   render() {
@@ -17,15 +17,15 @@ export default class MovieView extends React.Component {
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
-          <span calssName="value">{movie.Description}</span>
+          <span className="value">{movie.Description}</span>
         </div>
         <div className="movie-genre">
           <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
+          <span className="value">{movie.Genre}</span>
         </div>
         <div className="movie-director">
           <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
+          <span className="value">{movie.Director}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
